018276e65a5490a6d00ec6f57fa5b0ee06621751
fullstackmommy <nicole.budiman@gmail.com>
2019-03-05 06:17:34 +0800

    Clean up Core UI template

---
+++ src/components/Bookings/Bookings.jsx
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+
+class Bookings extends Component {
+
+    loading = () => <div className="animated fadeIn pt-1 text-center">Loading...</div>
+
+    render() {
+
+        return (
+            <div className="animated fadeIn">
+                Bookings
+            </div>
+        );
+    }
+}
+
+export default Bookings;
--- src/containers/DefaultLayout/DefaultFooter.js
+++ src/containers/DefaultLayout/DefaultFooter.js
@@ -1,8 +1,8 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 
 const propTypes = {
-  children: PropTypes.node,
+  children: PropTypes.node
 };
 
 const defaultProps = {};
@@ -11,12 +11,17 @@ class DefaultFooter extends Component {
   render() {
 
     // eslint-disable-next-line
-    const { children, ...attributes } = this.props;
+    const {
+      children,
+      ...attributes
+    } = this.props;
 
     return (
       <React.Fragment>
-        <span><a href="https://coreui.io">CoreUI</a> &copy; 2018 creativeLabs.</span>
-        <span className="ml-auto">Powered by <a href="https://coreui.io/react">CoreUI for React</a></span>
+        <span>Hostel App</span>
+        <span className="ml-auto">Powered by
+          <a href="https://coreui.io/react">CoreUI for React</a>
+        </span>
       </React.Fragment>
     );
   }
